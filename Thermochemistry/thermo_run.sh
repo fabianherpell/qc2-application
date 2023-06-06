5dd7bb3f80cb87d6c47d338dc60c099dd7705019
fabianherpell <s6faherp@uni-bonn.de>
2023-06-06 10:27:46 +0200

    thermo corr done ?

---
+++ Thermochemistry/thermo_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+for n in "nh3" "n2" "h2" ; do
+    pushd $n >/dev/null
+    for m in "opt_tpss"; do
+        pushd $m >/dev/null
+        # start opt tpss
+        thermo > thermo.out &
+        popd >/dev/null
+    done
+    popd >/dev/null
+done
+
+for n in "nh3" "n2" "h2" ; do
+    pushd $n >/dev/null
+    for m in "opt_mp2"; do
+        pushd $m >/dev/null
+        # start mps
+        thermo > thermo.out &
+        popd >/dev/null
+    done
+    popd >/dev/null
+done
+
+for n in "ch4" "h2o" "co" ; do
+    pushd $n >/dev/null
+    for m in "opt_tpss" ; do
+        pushd $m >/dev/null
+        # start opt tpss
+        thermo > thermo.out &
+        popd >/dev/null
+    done
+    popd >/dev/null
+done
