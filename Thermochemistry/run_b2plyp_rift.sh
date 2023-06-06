931528d766f50ffea0eade0235642254b47ec67e
fabianherpell <s6faherp@uni-bonn.de>
2023-06-06 12:09:30 +0200

    b2-plyp all done

--- Thermochemistry/run_b2plyp_rift.sh
+++ Thermochemistry/run_b2plyp_rift.sh
@@ -4,7 +4,7 @@ for n in "nh3" "n2" "h2" "ch4" "h2o" "co" ; do
     pushd $n >/dev/null
     for m in  "b2plyp"; do
         pushd $m >/dev/null
-        ridft > ridft.out &
+        ricc2 > ricc2.out &
         popd >/dev/null
     done
     popd >/dev/null
