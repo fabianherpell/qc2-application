fae826ff888f01a8e728737fc9e9a694c20a4fb6
fabianherpell <s6faherp@uni-bonn.de>
2023-06-08 16:43:11 +0200

    aoforce done thermo

--- Thermochemistry/run_thermo_rest.sh
+++ Thermochemistry/run_thermo_rest.sh
@@ -6,7 +6,7 @@ for n in "nh3" "n2" "h2" "ch4" "h2o" "co" ; do
     for m in  "b2plyp" "b3lyp" "ccsd_t" "mp2"; do
         pushd $m >/dev/null
         echo $m
-        #aoforce > aoforce.out &
+        thermo > thermo.out &
         popd >/dev/null
     done
     popd >/dev/null
