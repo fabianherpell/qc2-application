2c1ecf806f3feecbc429315403a2f5d943fbc2da
fabianherpell <s6faherp@uni-bonn.de>
2023-06-13 17:58:18 +0200

    noncovalent interaction

---
+++ Noncovalent_interaction/Ar/MP2/run_scan.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Choose directory here
+calc_dir=scan
+
+cd $calc_dir
+if [ -f ./results.dat ]
+then
+  rm results.dat
+fi
+
+read -r -d 'END' template <<-EOF
+$coord
+   0.00000000000000      0.00000000000000      DIST                  ar f
+   0.00000000000000      0.00000000000000      0.00000000000000      h f
+   0.00000000000000      0.00000000000000     -2.06945348098289      c
+   0.97576020317533      1.69006623336522     -2.75977586481614      h
+   0.97576020317533     -1.69006623336522     -2.75977586481614      h
+  -1.95152040635065      0.00000000000000     -2.75977586481614      h
+$end
+END
+EOF
+
+for dist in $(seq 4.5 0.25 15.00 | sed s/,/./)
+do
+
+  # Check for existence of folder
+  if [ -d $dist ]
+  then
+    rm -r $dist
+  fi
+  mkdir $dist
+  cp control $dist
+  pushd $dist
+  echo "$template" | sed "s/DIST/$dist/" > coord
+
+    jobex -ri -level cc2 -c 50
+
+  # Get final energy
+  e=$(grep "Total Energy" job.last | gawk '{print $4}')
+
+  # Write energy to a file
+  echo $dist $e >> ../results.dat
+  popd
+
+done
---
+++ Noncovalent_interaction/run_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for n in "Ar" "Kr" ; do
+    pushd $n >/dev/null
+    echo $n
+    for m in  "BLYP" "BLYP_D3" "MP2"; do
+        pushd $m >/dev/null
+        echo $m
+        bash run_scan.sh &
+        popd >/dev/null
+    done
+    popd >/dev/null
+done
+
