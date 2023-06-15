4b5359702db169fa2660f0fc8ae5098c048d08e3
fabianherpell <s6faherp@uni-bonn.de>
2023-06-15 10:54:58 +0200

    IR done

--- Copies/Noncovalent_interaction/IR_Spectroscopy/run_geo_opt.sh
+++ Copies/Noncovalent_interaction/IR_Spectroscopy/run_geo_opt.sh
@@ -3,7 +3,8 @@
 for n in "HF" "TPSS_D3" ; do
     pushd $n >/dev/null
     echo $n
-    aoforce > aoforce.out &
+        pushd $m >/dev/null
+        #jobex -ri > jobex.out &
     popd >/dev/null
 done
 
