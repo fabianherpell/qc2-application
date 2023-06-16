e8ad9a9e7187f2cc4bae8e852fa310bd57d1cbbd
fabianherpell <s6faherp@uni-bonn.de>
2023-06-16 09:52:08 +0200

    MP2 BS_COnv

--- Basis Set Convergence/run_ricc2_monomer.sh
+++ Basis Set Convergence/run_ricc2_monomer.sh
@@ -3,18 +3,18 @@
  for m in "Dimerization_Energy"; do
     pushd $m </dev/null
     echo $m 
-            for n in "HF" "MP2" "TPSS_D3"; do
+            for n in "MP2" ; do
             pushd $n >/dev/null
             echo $n
                 for p in "aug-cc-pVDZ" "aug-cc-pVQZ" "aug-cc-pVTZ" "cc-pVDZ" "cc-pVQZ" "cc-pVTZ" ; do
                 pushd $p >/dev/null
                 echo $p
-                        for q in "Dimer" "Monomer"; do 
+                        for q in "Monomer"; do 
                         pushd $q >/dev/null
                         echo $q
-                                 if test ! -f ridft.out;
+                                 if test ! -f ricc2.out;
                                  then
-                                 ridft > ridft.out &
+                                 ricc2 > ricc2.out &
                                  fi
                         popd >/dev/null
                         done
