5bb0b78569db333f945dc9b06cfca84784794709
fabianherpell <s6faherp@uni-bonn.de>
2023-06-16 15:26:34 +0200

    .

--- Spectroscopy/NMR/13C/GEO_OPT/GEO_OPT.sh
+++ Spectroscopy/NMR/13C/GEO_OPT/GEO_OPT.sh
@@ -3,6 +3,6 @@
 for n in "A" "B" "C" "D" "E" "F" "G" "Ar-1" "Ar-2" "Ar-3" "Ar-4" "TMS"; do
     pushd $n >/dev/null
     echo $n
-    orca *.inp > orca.out
+    orca *.inp > orca.out &
     popd >/dev/null
 done
