46a05d7ba13aac32870512353c3c632cc466c6dc
fabianherpell <s6faherp@uni-bonn.de>
2023-06-22 16:04:08 +0200

    auswertung reaction barrier done

--- Activation_Energies/Diels_Alder/grep_energy_path_DA.sh
+++ Activation_Energies/Diels_Alder/grep_energy_path_DA.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+    counter=0
 
     for m in "reaction" ; do
     pushd $m >/dev/null
 
+
+        awk -v counter=$counter 'NR == 2 || (NR-2) % 24 == 0 { print $0, counter++ }' stringfile.xyz0000 > ../plots/DA_path.dat
         #grep every x line (24 for diels) (16 for claisen)
-        awk 'NR == 2 || (NR-2) % 24 == 0' stringfile.xyz0000 > ../plots/claisen_path.dat
+        #awk 'NR == 2 || (NR-2) % 24 == 0' stringfile.xyz0000 > ../plots/claisen_path.dat
 
     popd >/dev/null
     done
