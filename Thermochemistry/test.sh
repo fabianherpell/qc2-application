6a5ea78b7266f0b85d26f80cca806fd383096a55
fabianherpell <s6faherp@uni-bonn.de>
2023-06-27 16:12:34 +0200

    energies for haber bosch

--- Thermochemistry/test.sh
+++ Thermochemistry/test.sh
@@ -1,36 +1,23 @@
- #!/bin/bash
+#!/bin/bash
  
- output_file="$(pwd)/Haber_Bosch_E"
+ output_file="$(pwd)/Haber_Bosch_test"
                 for p in "nh3" "n2" "h2"; do
                 pushd $p >/dev/null
                 echo $p
                 echo "$p " >> "$output_file"
 
-                # for ricc2 involved
-                        for q in "b2plyp" "mp2"; do 
-                        pushd $q >/dev/null
-                        echo $q
-                        echo -n "$q " >> "$output_file"  # Pfadname ($n) ohne Zeilenumbruch (-n)
-                        #grab FINAL MP2 ENERGY 
-                        input_file=$(find . -name "ricc2.out")
-                        pattern="Final MP2 energy\s*:\s*([-+]?[0-9]*\.?[0-9]+)"
-                        value=$(grep -Po "$pattern" "$input_file" | awk '{gsub(/[*]+/,"",$NF); print $NF}')
-                        echo  " $value" | sed 's/\*//g' >> "$output_file"  #durch >> nicht überschrieben
-                        echo $value         
-                        popd >/dev/null
-                        done
-
                 # for ridft.out only
                 for x in "b3lyp" ; do
                 pushd $x >/dev/null
                 echo $x
-                echo  -n "$x " >> "$output_file" 
+                printf " %s" "$x" >> "$output_file" 
+                #echo  -n "$x " >> "$output_file" 
                         #grab FINAL ridft ENERGY 
                         input_file=$(find . -name "ridft.out")
                         pattern="total energy"
                         value=$(grep -E "$pattern" "$input_file" | awk -F "=" '{gsub(/[|]/,"",$2); print $2}')
-                        #printf " %s" "$value" >> "$output_file" 
-                        echo  " $value" >> "$output_file" 
+                        printf " %s" "$value" >> "$output_file" 
+                        #echo  " $value" >> "$output_file" 
                         popd >/dev/null
                         done
 
