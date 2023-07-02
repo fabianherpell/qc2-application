0a7c64354750d5ea0d0646936758977261453796
fabianherpell <s6faherp@uni-bonn.de>
2023-07-02 18:37:17 +0200

    tpss prob done

--- Spectroscopy/IR_Spectroscopy/run_geo_opt.sh
+++ Spectroscopy/IR_Spectroscopy/run_geo_opt.sh
@@ -3,8 +3,8 @@
 for n in "HF_neu" "TPSS_D3_neu" ; do
     pushd $n >/dev/null
     echo $n
-    jobex -ri > jobex.out &
-    #aoforce > aoforce.out &
+    #jobex -ri > jobex.out &
+    aoforce > aoforce.out &
     popd >/dev/null
 done
 
